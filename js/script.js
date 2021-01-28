0787797dc48e678c92bb7d2f5741741a3ad8f18f
Rodolfo <rmedeiros@pm.me>
2021-01-28 15:43:21 -0300

    Main carousel done

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  //MENU
+  $('#nav-toggle').click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass('active');
+
+    $('.header-collapse').toggleClass('active');
+  });
+
+  // Carousel Principal
+  $('#carousel-principal').owlCarousel({
+    items: 1,
+    lazyLoad: true,
+    loop: true,
+    margin: 0,
+    nav: true,
+    navSpeed: 1000,
+    navText: [
+      '<i class="fas fa-chevron-left"></i>',
+      '<i class="fas fa-chevron-right"></i>',
+    ],
+    dots: true,
+    dotsSpeed: 1000,
+    //autoplay: true,
+    autoplaySpeed: 1000,
+    responsiveRefreshRate: 10,
+  });
+});
